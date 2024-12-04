f704bbc4b0e6330a345f2645278911aa3857ae10
chilli-somanyjuice <somanyjuice@gmail.com>
2024-12-04 21:45:08 +0900

    first commit

--- basicsr/version.py
+++ basicsr/version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Fri Aug 18 23:11:51 2023
-__version__ = "1.4.2"
-__gitsha__ = "unknown"
+# TIME: Wed Nov 27 23:54:42 2024
+__version__ = '1.4.2'
+__gitsha__ = '0459b42'
 version_info = (1, 4, 2)
